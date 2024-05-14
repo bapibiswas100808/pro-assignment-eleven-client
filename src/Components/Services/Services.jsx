48ffca936fd954c101d827c1c7442bd0a688ef4f
bapibiswas100808 <star.bappy@gmail.com>
2024-05-14 12:15:45 +0600

    search method added

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,8 +1,10 @@
 import { Link, useLoaderData } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
+import { useState } from "react";
 
 const Services = () => {
   const allServices = useLoaderData();
+  const [search, setSearch] = useState("");
   return (
     <div className="max-w-[1170px] mx-auto py-5 lg:py-10 px-3 lg:px-0">
       <Helmet>
@@ -14,57 +16,71 @@ const Services = () => {
         </h2>
       </div>
       <div>
+        <input
+          onChange={(e) => setSearch(e.target.value)}
+          type="text"
+          placeholder="Search here"
+          className="input input-bordered input-primary w-full mb-5"
+        />
+      </div>
+      <div>
         <div className="grid grid-cols-1  gap-5">
-          {allServices?.map((service, idx) => (
-            <div className="border rounded-lg" key={idx}>
-              <div className="hero">
-                <div className="hero-content flex-col lg:flex-row-reverse">
-                  <div className="w-full ">
-                    <img
-                      src={service?.photo}
-                      className=" rounded-lg shadow-2xl"
-                    />
-                  </div>
-                  <div className="w-full">
-                    <h1 className="text-2xl font-bold">
-                      {service.serviceName}
-                    </h1>
-                    <p className="py-6">{service.description}</p>
-                    <div className="flex items-center gap-10 mb-5">
-                      <p>
-                        <span className="text-lg font-bold mr-2">Price:</span>
-                        {service?.price}
-                      </p>
-                      <p>
-                        <span className="text-lg font-bold mr-2">
-                          Service Area:
-                        </span>
-                        {service?.serviceArea}
-                      </p>
-                    </div>
-                    <div className="flex flex-col md:flex-row lg:flex-row items-center gap-5">
-                      <p className="font-bold text-lg">Provider:</p>
+          {allServices
+            ?.filter((item) => {
+              return search.toLowerCase() === ""
+                ? item
+                : item.serviceName.toLowerCase().includes(search);
+            })
+            .map((service, idx) => (
+              <div className="border rounded-lg" key={idx}>
+                <div className="hero">
+                  <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div className="w-full ">
                       <img
-                        className="rounded-full h-12"
-                        src={service.providerImage}
-                        alt=""
+                        src={service?.photo}
+                        className=" rounded-lg shadow-2xl"
                       />
-                      <p>
-                        <span className="mr-2 font-semibold">Name:</span>
-                        {service.providerName}
-                      </p>
-                      <Link
-                        to={`/singleService/${service._id}`}
-                        className="btn project-btn"
-                      >
-                        View Details
-                      </Link>
+                    </div>
+                    <div className="w-full">
+                      <h1 className="text-2xl font-bold">
+                        {service.serviceName}
+                      </h1>
+                      <p className="py-6">{service.description}</p>
+                      <div className="flex items-center gap-10 mb-5">
+                        <p>
+                          <span className="text-lg font-bold mr-2">Price:</span>
+                          {service?.price}
+                        </p>
+                        <p>
+                          <span className="text-lg font-bold mr-2">
+                            Service Area:
+                          </span>
+                          {service?.serviceArea}
+                        </p>
+                      </div>
+                      <div className="flex flex-col md:flex-row lg:flex-row items-center gap-5">
+                        <p className="font-bold text-lg">Provider:</p>
+                        <img
+                          className="rounded-full h-12"
+                          src={service.providerImage}
+                          alt=""
+                        />
+                        <p>
+                          <span className="mr-2 font-semibold">Name:</span>
+                          {service.providerName}
+                        </p>
+                        <Link
+                          to={`/singleService/${service._id}`}
+                          className="btn project-btn"
+                        >
+                          View Details
+                        </Link>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
         </div>
       </div>
     </div>
